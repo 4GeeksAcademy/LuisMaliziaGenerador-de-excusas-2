b5919bde6b553a26687f272a74cb2456d7db1790
Luis Malizia <luisfmaliziam@gmail.com>
2024-11-20 12:38:21 +0100

    terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let quien = who[Math.floor(Math.random() * who.length)];
+  let accion = action[Math.floor(Math.random() * action.length)];
+  let que = what[Math.floor(Math.random() * what.length)];
+  let cuando = when[Math.floor(Math.random() * when.length)];
+
+  let excusa = `${quien} ${accion} ${que} ${cuando}.`;
+
+  document.getElementById("excuse").innerHTML = excusa;
 };
